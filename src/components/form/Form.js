e56f974f2d206c6c397439978cdf74813ca0fcfd
Ricardo Lima <navalimaricardo@gmail.com>
2021-12-30 12:41:02 -0600

    creacion de header y formulario para busqueda

---
+++ src/components/form/Form.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <form className='form'>
+            <div className='form__field'>
+                <label className='form__label'>Artista: </label>
+
+                <input 
+                    type="text"
+                    className='form__input'
+                    name="artist"
+                />
+            </div>
+
+            <div className='form__field'>
+                <label className='form__label'>Canción: </label>
+
+                <input 
+                    type="text"
+                    className='form__input'
+                    name="artist"
+                />
+            </div>
+
+            <div className='form__field'>
+                <input
+                    type="submit"
+                    value="Buscar"
+                    className='form__submit'
+                />
+            </div>
+        </form>
+    );
+}
+ 
+export default Form;
---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from '../form/Form';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1>Tus artistas favoritos en la palma de tu mano</h1>
+
+            <Form />
+        </header>
+    );
+}
+ 
+export default Header;
