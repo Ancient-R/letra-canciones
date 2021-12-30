2378173142758f2214b6f3988025b89e27870112
Ricardo Lima <navalimaricardo@gmail.com>
2021-12-30 15:06:07 -0600

    componente para mostrar errores, estilos css

---
+++ src/components/ui/error/Error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <p className='error fadein'>Verifica que los campos hayan sido llenados correctamente</p>
+    );
+}
+ 
+export default Error;
