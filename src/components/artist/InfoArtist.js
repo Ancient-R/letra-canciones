cf1d546bdfd414a38c1f321152f22d3a625f25f1
Ricardo Lima <navalimaricardo@gmail.com>
2021-12-30 14:03:13 -0600

    componentes para mostrar la información

---
+++ src/components/artist/InfoArtist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InfoArtist = () => {
+    return (
+        <div className="info__container">
+          <p className="info__title">Información del artista</p>
+        </div>
+    );
+}
+ 
+export default InfoArtist;
---
+++ src/components/letters/Letter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Letter = () => {
+    return (
+        <div className="letter__container">
+          <h1 className="letter__title">Letra de canción</h1>
+        </div>
+    );
+}
+ 
+export default Letter;
